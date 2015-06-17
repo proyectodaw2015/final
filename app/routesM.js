346925df024515c620f096853e91ab2bc5ad4e95
proyectodaw <proyectodaw@outlook.es>
2015-06-17 13:11:38 +0200

    commit

--- app/routesM.js
+++ app/routesM.js
@@ -5,8 +5,34 @@ var Music = require('../app/models/music');
 
 module.exports = function (app) {
 
-    app.get('/search', function (req, res) {
-        Music.find(req.params.id, function (error, music) {
+    app.get('/tutorial', isLoggedIn, function (req, res) {
+        {
+            res.render('guia.ejs', {
+                user: req.user // get the user out of session and pass to template
+            });
+        }
+        ;
+    });
+
+    app.get('/search', isLoggedIn, function (req, res) {
+
+        var title = req.params.searchName;
+        console.log(title);
+
+        Music.findOne({'title': title}, function (err, music) {
+            if (err)
+                res.send(err)
+            if (music) {
+                res.send(err)
+                req.flash('Upload', 'That name is already taken.');
+            } else {
+                req.flash('Upload', 'Succesfull insert in bd.');
+                console.log(music);
+                res.render('views2.ejs', {
+                    user: req.user, // get the user out of session and pass to template
+                    music: music
+                });
+            }
         });
     });
 
@@ -19,28 +45,49 @@ module.exports = function (app) {
         }
     });
 
-    app.post('/biblio/upload', isLoggedIn, function(req, res) {
+    app.get('/musics', isLoggedIn, function (req, res) {
+        {
+            Music.find(function (err, musics) {
+                if (!err) {
+                    console.log(musics);
+                    res.render('views.ejs', {
+                        musics: musics,
+                        user: req.user, // get the user out of session and pass to template
+                        message: req.flash('Upload')
+                    });
+                } else {
+                    res.statusCode = 500;
+                    console.log('Internal error(%d): %s', res.statusCode, err.message);
+                    res.send({error: 'Server error'});
+                }
+            });
+        }
+        ;
+    });
+
+    app.post('/biblio/upload', isLoggedIn, function (req, res) {
 
         var serverPath = '/music/' + req.body.userNombre + '.mp3';
         var title = req.body.userNombre;
         console.log(req.body.userNombre);
         console.log(serverPath);
 
-        Music.findOne({ 'title' :  title }, function(err, music) {
+        Music.findOne({'title': title}, function (err, music) {
             if (err)
                 res.send(err)
             if (music) {
                 res.send(err)
                 req.flash('Upload', 'That name is already taken.');
             } else {
+                console.log(music);
                 req.flash('Upload', 'Succesfull insert in bd.');
 
 
                 require('fs').rename(
                     req.files.userPhoto.path,
                     '/home/nico/WebstormProjects/nodibrary/views' + serverPath,
-                    function(error) {
-                        if(error) {
+                    function (error) {
+                        if (error) {
                             res.send({
                                 error: 'Ah crap! Something bad happened'
                             });
@@ -55,14 +102,14 @@ module.exports = function (app) {
 
                 // if there is no user with that email
                 // create the user
-                var newMusic           = new Music();
+                var newMusic = new Music();
 
                 // set the user's local credentials
-                newMusic.title    = title;
+                newMusic.title = title;
                 newMusic.url = serverPath;
 
                 // save the user
-                newMusic.save(function(err) {
+                newMusic.save(function (err) {
                     if (err)
                         throw err;
                 });
---
+++ views/js/views.js
@@ -0,0 +1,6 @@
+/**
+ * Created by nico on 17/06/15.
+ */
+$(document).ready(function() {
+    $('#table').dataTable();
+} );
